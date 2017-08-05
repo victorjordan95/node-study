241c50a45d078f0f0a08b9abd2412eeec3e965e7
Victor Jordan <victorjordan95@gmail.com>
2017-08-05 15:56:07 -0300

    criando rota json para consumir

--- produtosweb.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res){
-    res.end('<html><body><h1>Produtos</h1></body></html>')
-    console.log('deu certo');
-});
-server.listen(3000);
-console.log('servidor rodando');
